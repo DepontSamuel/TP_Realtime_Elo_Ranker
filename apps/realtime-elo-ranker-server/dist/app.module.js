9f5b3787ff504d29d818f94cd92a1e57bae5a2fe
Samuel Depont <depont.samuel@gmail.com>
2025-02-04 15:19:55 +0100

    ajout du debut de la bd

--- apps/realtime-elo-ranker-server/dist/app.module.js
+++ apps/realtime-elo-ranker-server/dist/app.module.js
@@ -8,6 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const typeorm_1 = require("@nestjs/typeorm");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const player_controller_1 = require("./player/player.controller");
@@ -22,7 +23,15 @@ let AppModule = class AppModule {
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [event_emitter_1.EventEmitterModule.forRoot()],
+        imports: [
+            event_emitter_1.EventEmitterModule.forRoot(),
+            typeorm_1.TypeOrmModule.forRoot({
+                type: 'sqlite',
+                database: 'db',
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: true,
+            }),
+        ],
         controllers: [
             app_controller_1.AppController,
             player_controller_1.PlayerController,
--- apps/realtime-elo-ranker-server/src/app.module.ts
+++ apps/realtime-elo-ranker-server/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlayerController } from './player/player.controller';
@@ -10,7 +11,15 @@ import { RankingController } from './ranking/ranking.controller';
 import { RankingEventsController } from './ranking/events/ranking.events.controller';
 
 @Module({
-  imports: [EventEmitterModule.forRoot()],
+  imports: [
+    EventEmitterModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [
     AppController,
     PlayerController,
